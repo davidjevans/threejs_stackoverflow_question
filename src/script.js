841320a761c6bbc6e2c6b95b5069c984ff0080c6
davidjevans <dezeumer@gmail.com>
2022-04-02 14:25:14 -0700

    david's changes

--- src/script.js
+++ src/script.js
@@ -1,28 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-
-// Debug
-const gui = new dat.GUI()
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-
-// Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
-
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+scene.background = new THREE.Color(0xeeeeee);
 
 // Lights
 
@@ -32,9 +18,14 @@ pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
-/**
- * Sizes
- */
+// Loading my glft file
+const gltfLoader = new GLTFLoader();
+const url = './assets/images/scene.gltf';
+gltfLoader.load(url, (model) => {
+    scene.add(model);
+});
+
+// Handling resizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -55,6 +46,7 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+
 /**
  * Camera
  */
@@ -66,8 +58,8 @@ camera.position.z = 2
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -87,13 +79,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
 
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
     // Update Orbital Controls
-    // controls.update()
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
